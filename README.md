a08d57f52f7dfc3c6a483b3620c705ded907ae35
kraksyx <blahblahusdme@gmail.com>
2025-06-20 20:40:50 +0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MyExam-RNAV-DOC-PT2-P2
+# MyExam-RNAV-DOC-PT1-BASIC-
---
+++ questions.js
@@ -0,0 +1,312 @@
+const questions = [
+  {
+    "question": "An airborne weather radar unit transmits a 5\u00b0 beam from a parabolic dish aerial reflector assembly. If the wavelength is 4 cm, the diameter of the dish is:",
+    "options": [
+      "20 cm",
+      "87.5 cm",
+      "87.5 ins",
+      "56.0 cm"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "On a colour radar, a bright red echo indicates:",
+    "options": [
+      "An area of strong wind shear",
+      "An area of extreme turbulence",
+      "Strong rising air currents",
+      "Heavy concentrations of liquid/solid water"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "An airborne weather radar, with a beam width of 4\u00b0 in azimuth, is used in mapping mode. At what maximum range would it be able to detect a 1 NM wide opening in a facing sea cliff?",
+    "options": [
+      "4 NM",
+      "60 NM",
+      "45 NM",
+      "15 NM"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "An aircraft flying at 25,000 ft is equipped with AWR. The beam width is 5\u00b0 with the radar tilted up at 3.5\u00b0. The radar is showing the top of a cloud return at 105 nm. The approximate height of the cloud is:",
+    "options": [
+      "14,300 ft",
+      "25,600 ft",
+      "30,300 ft",
+      "35,600 ft"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "The AWR can be used on the ground provided:",
+    "options": [
+      "The aircraft is clear of personnel, buildings and vehicles",
+      "The conical beam is selected",
+      "Maximum uplift is selected",
+      "The AWR must never be operated on the ground"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "On a colour radar, the greatest turbulence is likely in an area where the targets:",
+    "options": [
+      "Coloured red",
+      "Coloured magenta",
+      "Show a clearly defined hole",
+      "Show a rapid gradient of change from magenta to yellow"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Before commencing a flight the weather radar should:",
+    "options": [
+      "Be switched to stand-by but not used until airborne",
+      "Not be switched on until clear of buildings",
+      "Be switched to a range function after push back to make sure it is functioning",
+      "Be kept at stand-by until line up with the runway"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Weather radar is used by the pilot to assist in the:",
+    "options": [
+      "Detection and avoidance of all turbulence",
+      "Detection and avoidance of potentially turbulent cloud cells",
+      "Detection and determination of a route through active cloud formations",
+      "Detection of other aircraft through clouds"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "AWR in the contour mode is used for:",
+    "options": [
+      "Identifying rain bearing clouds",
+      "Long range mapping",
+      "Short range mapping",
+      "Identifying areas of maximum turbulence within a cloud"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "The iso-echo feature of an airborne weather radar can be used to detect:",
+    "options": [
+      "Wake turbulence",
+      "CAT",
+      "Turbulence in clouds",
+      "Areas of possible turbulence in clouds"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which of the following wavelengths would give the best penetration of weather?",
+    "options": [
+      "25 cm",
+      "50 cm",
+      "10 cm",
+      "3 cm"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "The DME ground transponder has a PRF of:",
+    "options": [
+      "2700 pulses per second",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "How many aircraft will DME accommodate before reaching saturation:",
+    "options": [
+      "100",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The time taken for the transmission of an interrogation pulse by a Distance Measuring Equipment (DME) to travel to the ground transponder and return to the airborne receiver was 2000 micro-second. The slant range from the ground transponder was:",
+    "options": [
+      "165 NM",
+      "186 NM",
+      "296 NM",
+      "330 NM"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "If a VOR station and a DME station, having different locations, are selected to provide a fix:",
+    "options": [
+      "Two difference IDs will have to be checked",
+      "Two positions, being ambiguous, will be presented",
+      "Two sets, with separate frequency control, are required in the aircraft",
+      "All 3 answers above are correct"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Groundspeed measurement using DME equipment is most accurate flying:",
+    "options": [
+      "From the station at long range",
+      "Over the station",
+      "Towards the station at short range",
+      "Past the station at short range"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Where a DME uses mechanical counters for displaying the range, continuous rotation of the counters means:",
+    "options": [
+      "The DME is in a search mode",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The DME in an aircraft, cruising at FL 210, fails to achieve lock on a DME at MSL at a range of 210 nm. The reason for this is:",
+    "options": [
+      "The aircraft is beyond line of sight range",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "What is the maximum distance apart a VOR and TACAN can be located and have the same identification?",
+    "options": [
+      "2000 m",
+      "60 m",
+      "600 m",
+      "6 m"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "When identifying a co-located VOR/DME the following signals are heard in the Morse code every 30 seconds?",
+    "options": [
+      "4 identifications in the same tone",
+      "4 identifications with the DME at a higher tone",
+      "4 identifications with the DME at a lower tone",
+      "No DME identification..."
+    ],
+    "correct": 1
+  },
+  {
+    "question": "A VOR and DME are frequency paired. The DME identification ends with Z. This denotes that:",
+    "options": [
+      "The two beacons are co-located",
+      "The beacons are supporting the same site but are not co-located",
+      "The beacons are greater than 2000 m apart",
+      "The beacons are at the same location but are more than 1000 m apart"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "In a certain VORTAC installation the VOR is coding STN and the DME is coding STZ. This means that the distance between the two beacons is in excess of:",
+    "options": [
+      "600 m",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "An aircraft at FL 360 is 10 nm plan range from a DME. The DME reading in the aircraft will be:",
+    "options": [
+      "11.7 nm",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The aircraft DME receiver is able to accept replies to its own transmission and reject replies to other aircraft interrogations because:",
+    "options": [
+      "Transmission frequencies are 63 MHz different for each aircraft",
+      "Pulse pairs are amplitude modulated...",
+      "Aircraft interrogation signals and...",
+      "Pulse pairs are discrete to a particular aircraft"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "A DME transmitter is operating in the search-for-lock phase. Which of the following statements is correct?",
+    "options": [
+      "The PRF increases and the range counters count down from the maximum",
+      "The PRF increases and the range counters count up from 300 to maximum",
+      "The PRF increases and the power is increased",
+      "The PRF increases, the range counters count down from maximum..."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Height error has the greatest effect on accuracy when an aeroplane is:",
+    "options": [
+      "Over the base line extensions at low altitude",
+      "At the base line bisector at low altitudes",
+      "At the base line bisector at high altitude",
+      "Over the base line extension at high altitude"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "DME is a radar type of facility and its maximum range is limited by:",
+    "options": [
+      "The pulse repetition interval",
+      "The height of the aeroplane",
+      "The transmitter power",
+      "The receiver sensitivity"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "In DME, interrogation and responses are separated by 63 MHz in order to:",
+    "options": [
+      "Avoid station saturation",
+      "Differentiate between precision and ordinary DME",
+      "Facilitate channel selection",
+      "Prevent self-triggering"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "A DME which is listed as operating on an 'X' channel is one which:",
+    "options": [
+      "Is paired exclusively with an ILS frequency",
+      "Is paired exclusively with a TACAN frequency",
+      "Responds on a frequency 63 MHz higher than the interrogation frequency",
+      "Responds on a frequency 63 MHz lower than the interrogation frequency"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "The nominal maximum coverage of a DME station is:",
+    "options": [
+      "150 nm at 20,000 ft",
+      "175 nm at 25,000 ft",
+      "190 nm at 25,000 ft",
+      "200 nm at 30,000 ft"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "How does the DME tell different aircraft apart:",
+    "options": [
+      "By using a jittered PRF",
+      "",
+      "",
+      ""
+    ],
+    "correct": 0
+  }
+];
